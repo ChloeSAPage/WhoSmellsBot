913565c85abeaca2ed27f79a4fcebdd82a6451ac
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-09-24 20:00:49 +0100

    allow user to specify role, store role in file

--- WhoSmells.py
+++ WhoSmells.py
@@ -2,34 +2,39 @@ import discord
 import os
 from dotenv import load_dotenv
 import random
+import json
+import functions
 
 
 load_dotenv()
 TOKEN = os.getenv("WHOSMELLSTOKEN")
-serverid_cummies = int(os.getenv("serverid_cummies"))
-IRL_ROLE_ID = int(os.getenv("IRL_ROLE_ID"))
-print(IRL_ROLE_ID, serverid_cummies)
-
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.guilds = True
 
-client = discord.Client(intents=intents)
+bot = discord.Client(intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
-    guild = client.get_guild(serverid_cummies)
-    roles = discord.utils.get(guild.roles, id=IRL_ROLE_ID)
-    global users
-    users = [m.name for m in roles.members]
+    print(f'We have logged in as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
+    
+    await functions.whosmells(message, bot)
+    
+    if message.content.lower().startswith("!"):
+        await functions.roleid_set(message)
 
-    if message.content.lower().startswith('who smells?'):
-        await message.channel.send(f"{random.choice(users)} smells!")
+@bot.event
+async def on_guild_join(guild):
+    for channel in guild.text_channels:
+        if channel.permissions_for(guild.me).send_messages:
+            await channel.send("Hey there! Please provide me with the role id of the " 
+                            "stinkers. Start your message with '!roleid' followed by the ID.")
+        break
 
-client.run(TOKEN)
+bot.run(TOKEN)
---
+++ functions.py
@@ -0,0 +1,44 @@
+import discord
+import json
+import random
+
+async def whosmells(message, bot):
+    if message.content.lower().startswith("who smells?"):
+            data = {}
+            guild_id = str(message.guild.id)
+            with open("roleid.json", "r") as file:
+                contents = file.read()
+                print(data)
+                data = json.loads(contents)
+                print(data)
+            if guild_id in data:
+                role_id = data[guild_id]
+                print(role_id)
+                guild = bot.get_guild(int(guild_id))
+                roles = discord.utils.get(guild.roles, id=int(role_id))
+                users = [m.name for m in roles.members]
+                await message.channel.send(f"{random.choice(users)} smells!")
+                return 0
+            else:
+                await message.channel.send("everyone smells!")
+                return 0
+
+
+async def roleid_set(message):
+    if message.content.lower().startswith("!roleid"):
+            try:
+                with open("roleid.json", "r+") as file:
+                    contents = file.read()
+                    data = json.loads(contents)
+                    guild_id = str(message.guild.id)
+                    role_id = message.content.split(" ")[1].strip()
+                    content = {guild_id:role_id}
+                    data.update(content)
+                    with open("roleid.json", "w") as file2:
+                        json.dump(data, file2)
+                        return 0
+                    
+            except FileNotFoundError:
+                with open("roleid.json", "x") as file:
+                    file.write("{}")
+                    return 0
